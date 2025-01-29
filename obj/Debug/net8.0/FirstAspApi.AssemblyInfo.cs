8bfe0af9426b772533667763f9a20a5d8d00c2ae
Sifiso Mawila <Sifiso@nebula.co.za>
2025-01-29 11:44:49 +0200

    Add .gitIgnore file

--- obj/Debug/net8.0/FirstAspApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstAspApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstAspApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b5f1d1a891f838b0b343a12932ddaa39033aa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstAspApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstAspApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
